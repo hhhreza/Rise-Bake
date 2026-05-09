dea6a87d985f352b3da2d832cb8203a719476980
Wedneszay <mazayafirasyan@gmail.com>
2026-05-09 12:48:34 +0700

    Update login.php

--- projek/login.php
+++ projek/login.php
@@ -5,57 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylebake.css">
 </head>
 <body>
-    
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">🥐 RiZe Bakery</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <main>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand ms-4" href="index.php">🥐 Rise & Bake</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto me-4 gap-1">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="produk.php">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about_us.php">About Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-
- <div class=" container card col-md-4 mt-5 shadow p-3 mb-5 bg-body rounded">
-        <form action="" class="text-center login"> 🥐 Login</form>
+        <div class=" container card col-md-4 mt-5 shadow p-3 mb-5 bg-body rounded">
+            <form action="" class="text-center login"> 🥐 Login</form>
 
             <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Masukkan email">
-             </div>
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Masukkan email">
+            </div>
 
             <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
             </div>
 
             <button type="submit" class="btn btn-primary" name="login">Login</button>
 
             <p class="card-text text-center mt-3">Belum memiliki akun? <a href="register.php">Daftar di sini</a></p>
-    </div>
-
+        </div>
+    </main>
     <footer class="footer">
-        <p>&copy; 2026 RiZe Bakery. All rights reserved.</p>
-        <p>📍 Babarsari josjis | 📧 hello@rizebakery.com</p>
+        <div class="wrap-footer">
+            <p class="copyright">&copy; 2026 Rise & Bake. All rights reserved.</p>
+            <p class="address">📍 Babarsari josjis | 📧 hello@rizebakery.com</p>
+        </div>
     </footer>
-
-
 </body>
 </html>
