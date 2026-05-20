5a6a91fcac63dc72813a88268c4557cf8c05a901
Wedneszay <mazayafirasyan@gmail.com>
2026-05-20 19:39:40 +0700

    Membuat file untuk memproses penambahan data produk

--- projek/proses_tambah.php
+++ projek/proses_tambah.php
@@ -1,22 +1,30 @@
 <?php
+session_start();
 include 'koneksi.php';
+
+// Kalau belum login atau bukan admin, tendang keluar
+if ($_SESSION['login'] != true || $_SESSION['role'] != 'admin') {
+    header("Location: index.php");
+    exit();
+}
 
 $kode_bakery = $_POST['kode_bakery'];
 $nama_produk = $_POST['nama_produk'];
 $harga = $_POST['harga'];
 $stok = $_POST['stok'];
 $deskripsi = $_POST['deskripsi'];
+$gambar = $_POST['gambar'];
 
 $cekKode = "SELECT * FROM list_produk WHERE kode_bakery = '$kode_bakery'";
 $resultKode = mysqli_query($koneksi, $cekKode);
 
 if (mysqli_num_rows($resultKode) > 0) {
-    $_SESSION['insertGagal'] = "Kode buku telah terdaftar";
+    $_SESSION['insertGagal'] = "Kode bakery telah terdaftar";
     header('Location: form_tambah.php');
     exit();
 }
 else {
-    $queryInsert = "INSERT INTO list_produk(id_produk, kode_bakery, nama_produk, harga, stok, deskripsi) VALUES ('', '$kode_bakery', '$nama_produk', '$harga', '$stok', '$deskripsi')";
+    $queryInsert = "INSERT INTO list_produk(id_produk, kode_bakery, nama_produk, harga, stok, deskripsi, gambar) VALUES ('', '$kode_bakery', '$nama_produk', '$harga', '$stok', '$deskripsi', '$gambar')";
     $resultInsert = mysqli_query($koneksi, $queryInsert);
     if ($resultInsert) {
         header('Location: data_produk.php');
