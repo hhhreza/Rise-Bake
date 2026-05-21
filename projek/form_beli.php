e313f2e725ddb79fd264226205c5e3938382f3fa
Wedneszay <mazayafirasyan@gmail.com>
2026-05-21 20:38:30 +0700

    Fix layout with adding margin bottom at form

--- projek/form_beli.php
+++ projek/form_beli.php
@@ -123,7 +123,7 @@ if (isset($_POST['submit'])) {
         </nav>
     </header>
 
-    <main class="container mt-5">
+    <main class="container mt-5 mb-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card shadow-sm">
